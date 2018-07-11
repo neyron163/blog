ca69b7d61def1797568229bcc5cc4fde66554d90
neyron163 <andrey163783@gmail.com>
2018-07-11 13:03:00 +0400

    added toggle on dark-theme

--- client/src/blog/components/header/Header.js
+++ client/src/blog/components/header/Header.js
@@ -3,13 +3,26 @@ import './header.css';
 
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: false
+        }
+
+        this.onClick = this.onClick.bind(this);
+    }
+    onClick(){
+        document.body.classList.toggle('dark-theme');
+        this.setState({ name: !this.state.name });
+
+    }
   render() {
     return (
-      <div className="content">
+      <div className="head">
         <header>
           <div className="nick-name">Welcome to Neyron's blog</div>
              <div>
-                 <button className="btn">Toggle on Dark Theme</button>
+                 <button className="btn" onClick={this.onClick}>Toggle on {this.state.name ? 'White' : 'Dark'} Theme</button>
             </div>
         </header>
       </div>
