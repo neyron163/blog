3c9bc819b8cd6911984b4a9d57b70fe3dc7c9aef
neyron163 <andrey163783@gmail.com>
2018-07-18 16:26:43 +0400

    social

--- client/src/blog/components/social/Social.js
+++ client/src/blog/components/social/Social.js
@@ -5,28 +5,12 @@ import './social.css';
 class Social extends Component {
     render() {
         return (
-            <div>
-                   <Button color='facebook'>
+            <div className="center">
+            <a href="https://www.facebook.com/profile.php?id=100009124048946" target="_blank">
+                    <Button color='facebook'>
                     <Icon name='facebook' /> Facebook
                     </Button>
-                    <Button color='twitter'>
-                    <Icon name='twitter' /> Twitter
-                    </Button>
-                    <Button color='google plus'>
-                    <Icon name='google plus' /> Google Plus
-                    </Button>
-                    <Button color='vk'>
-                    <Icon name='vk' /> VK
-                    </Button>
-                    <Button color='linkedin'>
-                    <Icon name='linkedin' /> LinkedIn
-                    </Button>
-                    <Button color='instagram'>
-                    <Icon name='instagram' /> Instagram
-                    </Button>
-                    <Button color='youtube'>
-                    <Icon name='youtube' /> YouTube
-                  </Button> 
+                    </a>
             </div>
         );
     }
