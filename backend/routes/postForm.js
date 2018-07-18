065ad550f0f14956facd9fecadb89bf2d0390805
neyron163 <andrey163783@gmail.com>
2018-07-18 13:57:20 -0400

    post form

--- backend/routes/postForm.js
+++ backend/routes/postForm.js
@@ -6,29 +6,26 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, './uploads/');
+        cb(null, './public/images');
     },
     filename: function(req, file, cb){
-        cb(null, new Date().toISOString() + file.originalname);
+        cb(null, file.originalname);
     }
 })
 const upload = multer({ storage: storage });
 
 const router = express.Router();
 
-router.post('/', upload.single('myFile'), (err, req, res) => {
-        if(err){
-            console.error(err);
-        }
+router.post('/', upload.single('myFile'), (req, res) => {
         // create new model and save it after got data
-        // const Article = new ArticleModel(req.body);
-        console.log(req.file)
-        // Article.save().then(() => {
+        const Article = new ArticleModel(req.body);
+        // console.log(req.file)
+        Article.save().then(() => {
             // we need send back single article of base
-            // ArticleModel.find((err, art) => {
-            //     res.json(art[art.length - 1])
-            // });
-        // });
+            ArticleModel.find((err, art) => {
+                res.json(art[art.length - 1])
+            });
+        });
 });
 
 
